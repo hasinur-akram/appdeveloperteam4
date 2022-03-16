9e0f44099529ff048ee07036b94d0e53a576021f
hasinur-akram <hasinurrahmanakram@gmail.com>
2022-03-16 16:54:02 +0000

    Home Page

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('App Developer Team4'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Wellcom our appdeeveloper t eam4'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
+import 'package:appdeveloperteam4/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
 }
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Welcome to Appdeveloperteam'),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
